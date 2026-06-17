da8e3e95d8b6e99d52d8d9228358d271e4ea8399
Ondrej Kubik <ondrej.kubik@canonical.com>
2026-06-17 17:06:40 +0100

    rust: client: add GLPv3 license headers
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- netplan-cli-rs/src/commands/info.rs
+++ netplan-cli-rs/src/commands/info.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2026 Canonical, Ltd.
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 //! `netplan info` – show available libnetplan features.
 //!
 //! Feature flags are extracted from `src/*.{h,c}` at build time by `build.rs`
--- netplan-cli-rs/src/yaml.rs
+++ netplan-cli-rs/src/yaml.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2026 Canonical, Ltd.
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 //! Minimal YAML parser producing [`serde_json::Value`].
 //!
 //! `libnetplan`'s `netplan_state_dump_yaml` always emits block-style
